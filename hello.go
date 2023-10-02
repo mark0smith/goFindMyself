c349ff8bf47b8eaff73e802c495e064c488cb066
mark0smith <admin@q12121212121q.com>
2023-10-03 01:10:14 +0800

    take arguments from input

--- hello.go
+++ hello.go
@@ -1,24 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 )
 
-func generateUniqueRandomNumbers(n, max int) []int {
+// generate `max` random numbers
+// if `unique` is ture, all numbers are unique
+func generateRandomNumbers(n, max int, unique bool) []int {
 	set := make(map[int]bool)
 	var result []int
 	for len(set) < n {
 		value := rand.Intn(max)
-		if !set[value] {
+		// fmt.Printf("[+] Run in %v Mode, Current Result is %d\n", unique, result)
+		if unique {
+			if !set[value] {
+				set[value] = true
+				result = append(result, value)
+			}
+		} else {
 			set[value] = true
 			result = append(result, value)
+
 		}
+		if len(result) >= n {
+			break
+		}
+
 	}
 	return result
 }
 
 func main() {
-	uniqueRandomNumbers := generateUniqueRandomNumbers(10, 100)
+	num := flag.Int("n", 10, "Number of Random Numbers")
+	maxium := flag.Int("m", 100, "Generated number should not bigger than ?")
+	unique := flag.Bool("u", false, "Should all generated number be unique?")
+	flag.Parse()
+
+	uniqueRandomNumbers := generateRandomNumbers(*num, *maxium, *unique)
 	fmt.Printf("Random Number is %d\n", uniqueRandomNumbers)
 }
