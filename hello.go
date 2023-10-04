defdc073c89c411339f2e3b8bce8fd3200f3f7f1
mark0smith <24486728+mark0smith@users.noreply.github.com>
2023-10-04 21:28:06 +0800

    add recall func

--- hello.go
+++ hello.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"math/rand"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -39,12 +41,44 @@ func generateRandomNumbers(n, max int, unique bool) []int {
 	return result
 }
 
+// recall func
+func checkRecall() {
+
+	fmt.Println("What do you remember?")
+
+	reader := bufio.NewReader(os.Stdin)
+	recallString, err := reader.ReadString('\n')
+	if err != nil {
+		panic(err)
+	}
+	recallString = strings.TrimSuffix(recallString, "\n")
+	fmt.Printf("\nYou have entered: %s\n", recallString)
+
+	toCheck := fmt.Sprintf("[%s]", recallString)
+	content, err := os.ReadFile("./log.txt")
+	if err != nil {
+		panic(err)
+	}
+	if strings.Contains(string(content), toCheck) {
+		fmt.Println("You have a correct memory!")
+	} else {
+		fmt.Println("Are you sure you remember it right?")
+	}
+
+}
+
 func main() {
 	num := flag.Int("n", 30, "Number of Random Numbers")
 	maxium := flag.Int("m", 100, "Generated number should not bigger than ?")
 	unique := flag.Bool("u", true, "Should all generated number be unique?")
 	remember := flag.Bool("r", false, "Should I remember all generated numbers?")
+	recall := flag.Bool("recall", false, "Do you find me?")
 	flag.Parse()
+
+	if *recall {
+		checkRecall()
+		return
+	}
 
 	datetime := time.Now()
 	datetimeFormatted := datetime.Format("2006-01-02 15:04:05")
@@ -57,8 +91,7 @@ func main() {
 
 		fil, err := os.OpenFile("./log.txt", os.O_CREATE|os.O_RDWR|os.O_APPEND, 0640)
 		if err != nil {
-			fmt.Printf("Error: %s\n", err)
-			return
+			panic(err)
 		}
 		defer fil.Close()
 		info := fmt.Sprintf("%s %d\n", datetimeFormatted, uniqueRandomNumbers)
